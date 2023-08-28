57afcb7ffc4b55de33dddc094e2cc24314c143b5
Ayodele <ayodelearogundade12@gmail.com>
2023-08-28 15:53:35 +0100

    add search functionality new branch
    Co-authored-by: Leghemo Precious <Prechyious@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,101 +1,122 @@
 "use strict"
 
-// const filenames = ["Data.png", "Creaticle expired.png", "Contest desc section.png"];
+const timeEl = document.querySelector("#time");
+const tempEl = document.querySelector("#day_temperature");
+let latEl = document.querySelector("#lat");
+let lonEl = document.querySelector("#lon");
 
-
-// const multipleImages = async (filenames) => {
-//     for (let filename of filenames) {
-        
-//         const response = await fetch(filename);
-//         const blob = await response.blob();
-//         // const imgUrl = URL.createObjectURL(blob);
-//         document.querySelector("#pic").src = URL.createObjectURL(blob);
-//         // imgEl.src = imgUrl;
-//         // console.log(imgEl)
-//     }
-// }
-// multipleImages(filenames)
-// .then(response => {console.log("nice")})
-// .catch(error => {console.error(error)})
-
-const timeArr = [];
-const hourlyTemp = [];
-// let latitude = "";
-// let longitude = 0;
-let timeZone = 0;
-let weatherData;
+const searchBarEl = document.querySelector("#search_bar");
+const searchSubmitBtnEl = document.querySelector("#search_submit_btn");
 
 
 
-const getWeather = async function () {
+
+const todaysDate = new Date();
+const currentHour = todaysDate.getHours();
+const BASE_URL = 'https://api.open-meteo.com/v1/forecast';
+ 
+
+// gets weather information and updates user interface
+
+const getWeather = async function (coordinates) {
+    const queryParams = new URLSearchParams({
+        latitude: coordinates.lat,
+        longitude: coordinates.lon,
+        hourly: 'temperature_2m',
+        forecast_days: 1
+    });
+
+    const url = `${BASE_URL}?${queryParams}`;
+
     try {
-        let response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=6.4541&longitude=3.3947&hourly=temperature_2m");
-        let data = await response.json();
-        return data
-        weatherData = data;
-        console.log(data);
-        const {latitude, longitude, time} = data;
-        console.log(longitude);
-        console.log()
-        // console.log(data.longitude);
-        timeArr.push(data.hourly.time)
-        hourlyTemp.push(data.hourly.temperature_2m)
-        // latitude = data.latitude;
-        // longitude = data.longitude;
-        // timeZone = data.timezone;
+        // fetch data from API
+        const response = await fetch(url, { headers: { 'User-Agent': USER_AGENT } })
+        const data = await response.json();
+
+        const temperature = data.hourly.temperature_2m;
+        
+        // checks if current time in hours is equak to its index and sets currentHour to time element in the UI. Des the same for the temperature. This works because both time and temperature from API have the same indexes in their respective arrays.
+        temperature.find((temp, index) => {
+            if (currentHour === index) {
+                currentHour < 11 ? timeEl.innerText = `Current time: ${currentHour}:00am` : timeEl.innerText = `Current time: ${currentHour}:00pm`;
+                
+                tempEl.innerText = `Current temperature: ${temp}${data.hourly_units.temperature_2m}`;
+            } else {
+                return 0
+            }
+        })
+
+        latEl.innerText = `Latitude: ${data.latitude}`;
+        lonEl.innerText = `Longitude: ${data.longitude}`;
+
+        // sets map to location from API
+        let map = L.map('map').setView([data.latitude, data.longitude], 8);
+
+        // Uses OpenStreetMap tiles
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(map);
+
+        // sets initial marker to 0, then sets it to latitude and longitude received from API
+        let marker = L.marker([0, 0]).addTo(map);
+        marker.setLatLng([data.latitude, data.longitude]);
     } catch (error) {
         console.log(error);
     }
-    // return data
 }
 
 
+// converts location string from user to coordinates using geocode and then calls getWeather function
 
-getWeather()
-    .then(data => {
-        weatherData = data;
-        console.log(data);
-        // ... process the data further
-    })
-    .catch(error => {
-        // Handle errors here
-        console.log(error);
+const NOMINATIM_API_URL = 'https://nominatim.openstreetmap.org/search';
+const USER_AGENT = 'SnowWeatherApp'; 
+
+async function getCoordinatesFromLocation(location) {
+    const queryParams = new URLSearchParams({
+        q: location,
+        format: 'json'
     });
-console.log(weatherData)
-// console.log(longitude)
-// console.log(latitude)
-// console.log(timeZone);
 
-// Example API date string: "2023-08-23T14:30:00Z"
+    const url = `${NOMINATIM_API_URL}?${queryParams}`;
 
-
-// fetch("https://api.open-meteo.com/v1/forecast?latitude=6.4541&longitude=3.3947&hourly=temperature_2m")
-// .then((response) => {return response.json()})
-// .then((data) => {
-//     const timeArr = data.hourly.time;
-//     timeArr.forEach((time) => {
-//         // console.log(time);
-//        console.log(timeConverter(time))
-        
-//     })
-//     // console.log(timeArr)
-// });
-
-// const humanReadableDate = apiDate.toLocaleDateString('en-US', options)
-
-// function timeConverter(timeStr) {
-//     // const humanReadableDate;
-//     const apiDate = new Date(timeStr);
-//     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-//     const humanReadableDate = apiDate.toLocaleDateString('en-US', options);
-//     console.log(humanReadableDate);
-//     return humanReadableDate
-// }
-
-// console.log(humanReadableDate)
-
-// const apiDateString = "2023-08-23T14:30:00Z";
+    try {
+        const response = await fetch(url, { headers: { 'User-Agent': USER_AGENT } });
+        if (response.ok) {
+            const data = await response.json();
+            if (data.length > 0) {
+                const coordinates = {
+                    lat: parseFloat(data[0].lat),
+                    lon: parseFloat(data[0].lon)
+                };
+                console.log(coordinates)
+                return coordinates;
+            }
+        }
+        throw new Error('Unable to geocode location');
+    } catch (error) {
+        console.error('Error geocoding location:', error);
+        return null;
+    }
+}
 
 
-// console.log(humanReadableDate);
-//     .catch((errro) => {console.log("Error: Something happened" + error)})
+// gets location from user input and calls getCoordinatesFromLocation function
+const getLocations = async (e) => {
+    e.preventDefault();
+    const userLocationStr = searchBarEl.value.toLowerCase();
+    console.log(userLocationStr);
+    // console.log(userLocationStr);
+    const coordinates = await getCoordinatesFromLocation(userLocationStr);
+    // console.log(coordinates);
+    
+    if (coordinates) {
+        getWeather(coordinates);
+        const latitude = coordinates.lat;
+        const longitude = coordinates.lon;
+    } else {
+        console.log('Coordinates not available');
+    }
+}
+
+searchSubmitBtnEl.addEventListener("click", getLocations);
